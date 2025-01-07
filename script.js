3a8c6449412872479d6d3633c666ce043f9c087a
Gabriel-Labariento <gabrielmatthew.labariento@gmail.com>
2025-01-07 08:56:13 +0800

    Console version finished.

--- script.js
+++ script.js
@@ -11,14 +11,31 @@ const playerMaker = (name, token) => {
     const playerToken = token;
     const playerName = name;
     let points = 0;
+    let takenSquares = [];
+
+
+    const makeMove = (position) => {
+        takenSquares.push(position);
+        gameBoard.placeToken(position, playerToken)
+    };
     
-    const makeMove = (position) => gameBoard.placeToken(position, playerToken);
     const getName = () => playerName;
     const addPoints = () => points++;
-    const getPoints = () => points;
-    const getToken = () => token;
-    
-    return {makeMove, addPoints, getPoints, getName, getToken}
+    const getScore = () => points;
+    const getToken = () => playerToken;
+    const getTakenSquares = () => takenSquares;
+    const resetTakenSquares = () => takenSquares = [];    
+    const getComputerChoice = () => {
+        let availableSquares = gameBoard.getAvailableSquares();
+        return availableSquares[Math.floor(Math.random() * availableSquares.length)]; 
+    }
+    const getHumanChoice = () => {
+        let availableSquares = gameBoard.getAvailableSquares();
+        return Math.floor(Math.random() * availableSquares.length);
+    }
+
+
+    return {makeMove, addPoints, getScore, getName, getToken, getComputerChoice, getHumanChoice, getTakenSquares, resetTakenSquares}
 };
 
 const gameBoard = (function () {
@@ -47,30 +64,101 @@ const gameBoard = (function () {
         )
     }
 
-    const placeToken = (function(position, token) {
-        let row = (position >= 3) ? Math.floor(position / 3) : 0; 
-        let column = position - (3 * row);
-
+    const placeToken = (position, token) => {
+        if (position < 0 || position > 8) return false;
+        const row = Math.floor(position / 3); 
+        const column = position % 3;
         board[row][column].setValue(token);
-    })
-    return{getBoard, printBoard, resetBoard, placeToken};
+    }
+
+    const getAvailableSquares = () => {
+        let availableSquares = [];
+        for (let i = 0; i < rows; i++){
+            for (let j = 0; j < columns; j++){
+                if (board[i][j].getValue() === null){
+                    let squarePosition = (i * 3) + j;
+                    availableSquares.push(squarePosition)
+                }
+            }
+        }
+        return availableSquares;
+    }
+
+    return{getBoard, printBoard, resetBoard, placeToken, getAvailableSquares};
 })();
 
 const gameMaster = (function(){
     const playerOne = playerMaker("playerOne", "X");
     const playerTwo = playerMaker("playerTwo", "O");
-    
-    playerOne.makeMove(0);
-    playerTwo.makeMove(1);
-    playerOne.makeMove(2);
-    playerTwo.makeMove(3);
-    playerOne.makeMove(4);
-    playerTwo.makeMove(5);
-    playerOne.makeMove(6);
-    playerTwo.makeMove(7);
-    playerOne.makeMove(8);
-    gameBoard.printBoard(); 
+    const maxScore = 5;
 
+    const gameHasWinner = () => {
+        return (playerOne.getScore() >= maxScore || playerTwo.getScore() >= maxScore)  
+    }
+
+    const roundHasWinner = () => {
+        const winningPositions = [
+            [0,1,2], [3,4,5], [6,7,8], // Horizontal wins
+            [0,3,6], [1,4,7], [2,5,8], // Vertical wins
+            [0,4,8], [2,4,6]          // Diagonal wins
+        ]
+        
+        let playerOneSquares = playerOne.getTakenSquares().sort((a,b) => a - b)
+        let playerTwoSquares = playerTwo.getTakenSquares().sort((a,b) => a - b)
+
+        for (let position of winningPositions){
+            if (position.every(num => playerOneSquares.includes(num))) return 0;
+            else if (position.every(num => playerTwoSquares.includes(num))) return 1;
+        }
+        return -1;
+        
+    }
+
+    const playRound = () => {
+        gameBoard.resetBoard();
+        playerOne.resetTakenSquares();
+        playerTwo.resetTakenSquares();
+
+        let turnCounter = 1;
+        while (roundHasWinner() === -1){
+            if (turnCounter % 2 == 0){
+                playerTwo.makeMove(playerTwo.getComputerChoice());
+            } else {
+                playerOne.makeMove(playerOne.getComputerChoice());
+            }
+            turnCounter++;
+            availableSquares = gameBoard.getAvailableSquares();
+            if (availableSquares.length === 0) {
+                console.log("Tie round");
+                return;
+            }
+            gameBoard.printBoard();
+        }
+
+        if (roundHasWinner !== -1){
+            (roundHasWinner() === 0 ) ? playerOne.addPoints() : playerTwo.addPoints();
+        }
+
+        
+    }
+
+    const playGame = () => {
+        console.log("New Game Starting")
+        let roundCounter = 1;
+        while (!gameHasWinner()){
+            console.log(`Round ${roundCounter} begins!`)
+            playRound();
+            roundCounter++;
+            console.log(`Player One: ${playerOne.getScore()}`)
+            console.log(`Player Two: ${playerTwo.getScore()}`)  
+        }
+        
+        let winner = (playerOne.getScore() > playerTwo.getScore()) ? playerOne.getName() : playerTwo.getName();
+
+        console.log(`${winner} wins!`)
+    }
+
+    return {playGame}
 })();
 
-
+gameMaster.playGame();
